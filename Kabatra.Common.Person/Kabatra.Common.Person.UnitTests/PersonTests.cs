bfc6f85c46156a72b37402cb1c235c6877be0861
Kevin Kabatra <kevin@lsretail.com>
2021-08-30 10:01:12 -0400

    Beginning to write test for Person

---
+++ Kabatra.Common.Person/Kabatra.Common.Person.UnitTests/PersonTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Kabatra.Common.Person.UnitTests
+{
+    using TestData;
+    using Xunit;
+
+    public class PersonTests
+    {
+        [Fact]
+        public void CanCreatePerson()
+        {
+            var person = new Person(TestPersonData.KevinFirstName, TestPersonData.KevinLastName);
+
+            Assert.NotNull(person);
+        }
+    }
+}
---
+++ Kabatra.Common.Person/Kabatra.Common.Person.UnitTests/TestData/TestPersonData.cs
@@ -0,0 +1,8 @@
+﻿namespace Kabatra.Common.Person.UnitTests.TestData
+{
+    public class TestPersonData
+    {
+        public static readonly string KevinFirstName = "Kevin";
+        public static readonly string KevinLastName = "Kabatra";
+    }
+}
